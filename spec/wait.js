3851838935884bd11650256cfed55bfa4da105c2
Yoshiya Hinosawa <stibium121@gmail.com>
2016-01-16 00:03:43 +0900

    Add wait test

--- spec/wait.js
+++ spec/wait.js
@@ -4,7 +4,36 @@ describe('wait', () => {
 
     it('returns a promise', () => {
 
-        expect(wait).to.be.a('function')
+        expect(wait()).to.be.instanceof(Promise)
+
+    })
+
+    it('resolves in the given milliseconds', () => {
+
+        let foo = false
+        let bar = false
+
+        setTimeout(() => { foo = true }, 600)
+        setTimeout(() => { bar = true }, 400)
+
+        wait(500).then(() => {
+
+            expect(foo).to.be.false
+            expect(bar).to.be.true
+
+        })
+
+    })
+
+    it('resolves with the given object', () => {
+
+        let a = {}
+
+        wait(0, a).then((x) => {
+
+            expect(a).to.equal(x)
+
+        })
 
     })
 
