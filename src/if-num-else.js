a067c555fb5aea40c25b543caa2da7a4b8470ccc
Yoshiya Hinosawa <stibium121@gmail.com>
2016-01-27 22:09:41 +0900

    Add if-num-else

---
+++ src/if-num-else.js
@@ -0,0 +1,8 @@
+/**
+ * Shorthand for `typeof num === 'number' ? num : defaultValue`.
+ *
+ * @param {object} num The number or anthing
+ * @param {number} defaultValue The default value
+ * @return {number}
+ */
+export default (num, defaultValue) => typeof num === 'number' ? num : defaultValue
