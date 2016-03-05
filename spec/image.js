83b4cfa3fb492d226bca9f9b821f0f1505c87032
Yoshiya Hinosawa <stibium121@gmail.com>
2016-03-05 13:34:36 +0900

    Add image

---
+++ spec/image.js
@@ -0,0 +1,32 @@
+import {Image} from '../src'
+
+describe('Image', () => {
+    'use strict'
+
+    describe('apply', () => {
+
+        it('applies the src and style to the jquery img element', () => {
+
+            const img = $('<img />')
+
+            new Image('foo.svg', false, false).apply(img)
+            expect(img.attr('src')).to.equal('foo.svg')
+            expect(img.css('transform')).to.equal('scale(1, 1)')
+
+            new Image('foo.svg', false, true).apply(img)
+            expect(img.attr('src')).to.equal('foo.svg')
+            expect(img.css('transform')).to.equal('scale(1, -1)')
+
+            new Image('foo.svg', true, false).apply(img)
+            expect(img.attr('src')).to.equal('foo.svg')
+            expect(img.css('transform')).to.equal('scale(-1, 1)')
+
+            new Image('foo.svg', true, true).apply(img)
+            expect(img.attr('src')).to.equal('foo.svg')
+            expect(img.css('transform')).to.equal('scale(-1, -1)')
+
+        })
+
+    })
+
+})
--- src/image.js
+++ src/image.js
@@ -1,1 +1,48 @@
-export default class Image {}
+/**
+ * The image object
+ */
+export default class Image {
+
+    /**
+     * @constructor
+     * @param {string} src The url of the image
+     * @param {boolean} mirrorX If the image is mirrored by x-axis
+     * @param {boolean} mirrorY If the image is mirrored by y-axis
+     */
+    constructor(src, mirrorX, mirrorY) {
+
+        this.src = src
+        this.mirrorX = mirrorX
+        this.mirrorY = mirrorY
+
+        this.scaleX = this.mirrorX ? -1 : 1
+        this.scaleY = this.mirrorY ? -1 : 1
+
+    }
+
+    /**
+     * Apply the image src and style to the element.
+     *
+     * @param {jQuery} elem The element to apply the image info (needs to be <img> jquery object)
+     */
+    apply(elem) {
+
+        elem.css('transform', this.makeTransform())
+
+        elem.attr('src', this.src)
+
+    }
+
+    /**
+     * Makes the transform style.
+     *
+     * @private
+     * @return {string}
+     */
+    makeTransform() {
+
+        return 'scale(' + this.scaleX + ', ' + this.scaleY + ')'
+
+    }
+
+}
